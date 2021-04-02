9b7695928bdd553766649defbda97b1059525f45
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-04-02 15:47:56 +0300

    deleted unused constants

--- ChessBoardView/DefaultSettings.cs
+++ ChessBoardView/DefaultSettings.cs
@@ -10,7 +10,5 @@ namespace ChessBoardView
     {
         public const int DEFAULT_BOARD_LONG = 8;
         public const int DEFAULT_BOARD_WHIDTH = 8;
-        public const ConsoleKey START_KEY = ConsoleKey.Enter;
-        public const ConsoleKey EXIT_KEY = ConsoleKey.Escape;
     }
 }
