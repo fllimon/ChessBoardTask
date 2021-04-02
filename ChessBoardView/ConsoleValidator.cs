6938d7e2dc3a17ddefd16ee0ac0d684a9f2c4c4d
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-04-02 12:19:51 +0300

    add method GetValidData

--- ChessBoardView/ConsoleValidator.cs
+++ ChessBoardView/ConsoleValidator.cs
@@ -8,5 +8,21 @@ namespace ChessBoardView
 {
     class ConsoleValidator
     {
+        public bool GetValidData(int longBoard, int whidthBoard)
+        {
+            bool isOk = true;
+
+            if (IsNotCorrectValue(longBoard, whidthBoard))
+            {
+                return isOk = false;
+            }
+
+            return isOk;
+        }
+
+        private bool IsNotCorrectValue(int longBoard, int whidthBoard)
+        {
+            return ((longBoard < 2) || (whidthBoard < 2));
+        }
     }
 }
